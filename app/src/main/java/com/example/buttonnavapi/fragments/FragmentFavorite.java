68e30f26c995eac6dd0c3e41d13fefcce3219de3
Eka Fitri Ramadani <eka.efira@gmail.com>
2021-05-09 21:59:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/buttonnavapi/fragments/FragmentFavorite.java
@@ -0,0 +1,35 @@
+package com.example.buttonnavapi.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.buttonnavapi.R;
+
+public class FragmentFavorite extends Fragment {
+
+    public FragmentFavorite(){
+
+    }
+    public static FragmentFavorite newInstance(){
+        FragmentFavorite fragment = new FragmentFavorite();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/buttonnavapi/networks/Consts.java
@@ -0,0 +1,12 @@
+package com.example.buttonnavapi.networks;
+
+public class Consts {
+
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    public static final String APIKEY = "9b2f5c328790f1c35495ae7431f7c63a";
+    public static final String LANGUAGE = "en-US";
+    public static final String PAGE = "1";
+    public static final String IMAGEBASEURL = "https://image.tmdb.org/t/p/w300/";
+    public static final String APIERROR = "DEBUG:API ERROR";
+
+}
